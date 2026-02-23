555a25dc7129ed8ec82f4e345afab996eff0714b
Ahmad Khalifa <vexeduxr@FreeBSD.org>
2026-02-23 06:47:06 +0200

    tools/stddef.h: fix non-macos build

--- tools/build/stddef.h
+++ tools/build/stddef.h
@@ -36,14 +36,14 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-#pragma once
+
 #include_next <stddef.h>
 
 #ifndef _PTRADDR_T_DECLARED
 #ifdef __PTRADDR_TYPE__
 typedef	__PTRADDR_TYPE__	ptraddr_t;
 #else
-typedef	size_t			ptraddr_t;
+typedef	__SIZE_TYPE__		ptraddr_t;
 #endif
 #define _PTRADDR_T_DECLARED
 #endif
